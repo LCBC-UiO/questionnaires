8dee948f8ad89da43845e87ba749b2b458490989
Athanasia Mo Mowinckel <athanasm@uio.no>
2022-03-04 14:29:59 +0100

    update gds functions

--- R/gds_functions.R
+++ R/gds_functions.R
@@ -17,7 +17,7 @@ gds_values = function(yes = 1, no = 0){
 }
 
 
-#' Binerise GDS values
+#' Binarise GDS values
 #' 
 #' internal function to make all "yes" answers
 #' equal to 1, and all "no" to 0. This for
@@ -29,14 +29,13 @@ gds_values = function(yes = 1, no = 0){
 #' @return vector of 0's and 1's
 #' @export
 #' @family gds_functions
+#' @importFrom dplyr case_when
 #' @examples
 #' gds_binary(c(1,1,0,NA,1), gds_values(1,0))
 #' gds_binary(c("y","y","n",NA,"y"), gds_values(yes = "y", no = "n"))
 gds_binary <- function(x, values = gds_values()){
-  
   stopifnot(is_gds_values(values))
-  
-  dplyr::case_when(
+  case_when(
     x == values$no ~ 1,
     x == values$yes ~ 0,
     TRUE ~ NA_real_
@@ -49,19 +48,20 @@ gds_binary <- function(x, values = gds_values()){
 #' 
 #' @param reverse reverse logic
 #' @inheritParams gds_compute_sum
-#' 
+#' @importFrom dplyr matches mutate across
 #' @family gds_functions
 gds_alter_values <- function(data, values = gds_values(), reverse = FALSE,
-                               cols = dplyr::matches("01$|05$|07$|09$|15$|19$|21$|27$|29$|30$")){
-
+                             cols = matches("01$|05$|07$|09$|15$|19$|21$|27$|29$|30$")){
+  
   if(reverse){
     values <- rev(unname(values))
     names(values) <- c("yes", "no")
   }
   
-  dplyr::mutate_at(data, 
-                   dplyr::vars( {{cols}} ), 
-                   gds_binary, values = values)
+  mutate(data, 
+         across({{cols}}, 
+         ~ gds_binary(.x, values = values))
+  )
 }
 
 
@@ -78,23 +78,18 @@ gds_alter_values <- function(data, values = gds_values(), reverse = FALSE,
 #' @param values named vector of 2 providing the coding for Yes and No answers c(Yes = 1, No = 2)
 #' @export
 #' @return numeric
+#' @importFrom dplyr matches select
 #' @family gds_functions
 gds_compute_sum <- function(data, 
-                            cols = dplyr::matches("GDS_[0-3][0-9]$"),
+                            cols = dplyr::matches("[0-3][0-9]$"),
                             cols_rev = dplyr::matches("01$|05$|07$|09$|15$|19$|21$|27$|29$|30$"),
                             values = gds_values()
 ){
-  
   stopifnot(is_gds_values(values))
-  
-  tmp <- dplyr::select(data, {{cols}} )
-  
+  tmp <- select(data, {{cols}} )
   tmp <- gds_alter_values(tmp, values, reverse = FALSE, cols_rev)
   tmp <- gds_alter_values(tmp, values, reverse = TRUE, -cols_rev)
-  
-  tt <- rowSums(tmp)
-
-  tt
+  rowSums(tmp)
 }
 
 #' Factorise the GDS sum
@@ -106,13 +101,12 @@ gds_compute_sum <- function(data,
 #' @family gds_functions
 gds_factorise <- function(gds_sum){
   tmp <- case_when(
-    gds_sum <= 9 ~ "Normal",
-    gds_sum >= 10  & gds_sum <= 19 ~ "Mild depression",
-    gds_sum > 19 ~ "Severe depression"
+    gds_sum <= 9 ~ "normal",
+    gds_sum >= 10  & gds_sum <= 19 ~ "mild depression",
+    gds_sum > 19 ~ "severe depression"
   )
-  
   factor(tmp,
-         levels = c("Normal", "Mild depression", "Severe depression"),
+         levels = c("normal", "mild depression", "severe depression"),
          ordered = TRUE)
 }
 
@@ -123,41 +117,42 @@ gds_factorise <- function(gds_sum){
 #' @param cols GDS data columns
 #' @param cols_rev Columns for reversal of binary code
 #' @param values named vector of 2 providing the coding for Yes and No answers c(Yes = 1, No = 2)
-#' @param keep_all logical, append to data.frame
+#' @template keep_all 
+#' @template prefix
 #' @family gds_functions
 #' @return data frame
 #' @export
-#' @importFrom dplyr mutate bind_cols select one_of
+#' @importFrom dplyr mutate bind_cols select one_of matches
 gds_compute <- function(data, 
-                        cols = dplyr::matches("GDS_[0-9][0-9]$"),
+                        cols = dplyr::matches("[0-9][0-9]$"),
                         cols_rev = dplyr::matches("01$|05$|07$|09$|15$|19$|21$|27$|29$|30$"),
                         values = gds_values(),
+                        prefix = "gds_",
                         keep_all = TRUE){
   
   stopifnot(is_gds_values(values))
   
-  tmp <- data.frame(GDS = gds_compute_sum(data = data, 
+  tmp <- data.frame(sum = gds_compute_sum(data = data, 
                                           cols = cols,
                                           cols_rev = cols_rev,
                                           values = values)
   )
-  
-  tmp <- mutate(tmp, GDS_Coded = gds_factorise(GDS))
+  tmp <- mutate(tmp, coded = gds_factorise(sum))
+  if(!is.null(prefix)){
+    tmp <- rename_all(tmp, ~paste0(prefix, .x))
+  }
   
   if(keep_all){
-    bind_cols(select(data,
-                     -one_of(names(data)[names(data) %in% names(tmp)])), 
-              tmp)
-  }else{
-    tmp
+    tmp <- bind_cols(data, tmp)
   }
+  tmp
 }
 
 is_gds_values <- function(x){
   j <- length(x) == 2
   k <- all(names(gds_values("Yes", "No")) == c("yes", "no"))
-  
   all(c(k,j))
 }
 
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("GDS"))
+if(getRversion() >= "2.15.1")  
+  utils::globalVariables(c("sum"))
--- tests/testthat/test-gds_functions.R
+++ tests/testthat/test-gds_functions.R
@@ -1,4 +1,4 @@
-load(paste0(test_path(), "/data/gds.rda"))
+gds <- read.delim(test_path("data/gds.tsv"))
 
 test_that("check binary", {
   
@@ -17,22 +17,22 @@ test_that("check binary", {
 
 test_that("check gds_alter_values",{
   
-  expect_error(gds_alter_values(gds, cols = GDS_01, value = 1),
+  expect_error(gds_alter_values(gds, cols = gds_01, value = 1),
                "is not TRUE")
   
   
-  tmp <- gds_alter_values(gds, cols = GDS_01, value = gds_values(1, 0))
-  expect_equal(tmp$GDS_01,
+  tmp <- gds_alter_values(gds, cols = gds_01, value = gds_values(1, 0))
+  expect_equal(tmp$gds_01,
                rep(1,10)
   )
   
-  tmp <- gds_alter_values(gds, cols = GDS_05, value = gds_values(1, 0))
-  expect_equal(tmp$GDS_05,
+  tmp <- gds_alter_values(gds, cols = gds_05, value = gds_values(1, 0))
+  expect_equal(tmp$gds_05,
                c(rep(1,3), 0, rep(1,5),0)
   )
   
-  tmp <- gds_alter_values(gds, cols = GDS_05, reverse = TRUE, value = gds_values(1, 0))
-  expect_equal(tmp$GDS_05,
+  tmp <- gds_alter_values(gds, cols = gds_05, reverse = TRUE, value = gds_values(1, 0))
+  expect_equal(tmp$gds_05,
                c(rep(0,3), 1, rep(0,5),1)
   )
 })
@@ -56,16 +56,16 @@ test_that("Check sum", {
 test_that("Check factor",{
   expect_equal(gds_factorise(gds_compute_sum(gds, values = gds_values(yes = 1, no = 0))),
                structure(c(1L, 2L, 1L, 2L, 1L, 2L, 2L, 2L, 1L, 2L), 
-                         .Label = c("Normal", 
-                                    "Mild depression", 
-                                    "Severe depression"), 
+                         .Label = c("normal", 
+                                    "mild depression", 
+                                    "severe depression"), 
                          class = c("ordered", "factor"))
   )
   
   expect_equal(as.character(gds_factorise(gds_compute_sum(gds, values = gds_values(yes = 1, no = 0)))),
-               c("Normal", "Mild depression", "Normal", "Mild depression", "Normal", 
-                 "Mild depression", "Mild depression", "Mild depression", "Normal", 
-                 "Mild depression"))
+               c("normal", "mild depression", "normal", "mild depression", "normal", 
+                 "mild depression", "mild depression", "mild depression", "normal", 
+                 "mild depression"))
   
 })
 
@@ -75,21 +75,22 @@ test_that("Check compute", {
   expect_equal(names(gds_compute(gds, 
                                  values = gds_values("Yes", "No"),
                                  keep_all = FALSE)),
-               c("GDS", "GDS_Coded")
+               c("gds_sum", "gds_coded")
   )
   
   expect_equal(names(gds_compute(gds, 
                                  values = gds_values("Yes", "No"),
+                                 prefix = "gds2_",
                                  keep_all = TRUE)),
-               c("GDS_01", "GDS_02", "GDS_03", "GDS_04", "GDS_05", "GDS_06", 
-                 "GDS_07", "GDS_08", "GDS_09", "GDS_10", "GDS_11", "GDS_12", "GDS_13", 
-                 "GDS_14", "GDS_15", "GDS_16", "GDS_17", "GDS_18", "GDS_19", "GDS_20", 
-                 "GDS_21", "GDS_22", "GDS_23", "GDS_24", "GDS_25", "GDS_26", "GDS_27", 
-                 "GDS_28", "GDS_29", "GDS_30", "GDS", "GDS_Coded")
+               c("gds", "gds_coded","gds_01", "gds_02", "gds_03", "gds_04", "gds_05", "gds_06", 
+                 "gds_07", "gds_08", "gds_09", "gds_10", "gds_11", "gds_12", "gds_13", 
+                 "gds_14", "gds_15", "gds_16", "gds_17", "gds_18", "gds_19", "gds_20", 
+                 "gds_21", "gds_22", "gds_23", "gds_24", "gds_25", "gds_26", "gds_27", 
+                 "gds_28", "gds_29", "gds_30", "gds2_sum", "gds2_coded")
   )
   
   expect_equal(ncol(gds_compute(gds, keep_all = TRUE)),
-               32
+               34
   )
 })
 
