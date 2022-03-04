88c9912477ec5b8c2c52f7d0c221423fafd9aafa
Athanasia Mo Mowinckel <athanasm@uio.no>
2022-03-04 14:32:24 +0100

    update psqi functions

--- R/psqi_functions.R
+++ R/psqi_functions.R
@@ -1,70 +1,75 @@
 
-#' Compute time in bed
-#' @param bedtime column name with bedtime (HH:MM:SS) [PSQI_01]
-#' @param risingtime column name with rising time (HH:MM:SS) [PSQI_03]
+#' compute time in bed
+#' @param bedtime column name with bedtime (HH:MM:SS) [psqi_01]
+#' @param risingtime column name with rising time (HH:MM:SS) [psqi_03]
+#' @param risingtime_func function to convert time to \code{Period}
+#' @param bedtime_func function to convert time to \code{Period}
 #' @importFrom dplyr if_else
 #' @importFrom lubridate hms period_to_seconds hours
-compute_time_in_bed <- function(risingtime, bedtime){
-  tmp <- hms(risingtime, quiet = T) - hms(bedtime, quiet = T)
+compute_time_in_bed <- function(risingtime, bedtime, 
+                                risingtime_func = lubridate::hm,
+                                bedtime_func = lubridate::hm){
+  tmp <- risingtime_func(risingtime, quiet = TRUE) - bedtime_func(bedtime, quiet = TRUE)
   period_to_seconds(if_else(as.numeric(tmp) < 0, hours(24) + tmp, tmp))/3600
 }
 
-#' Compute component PSQI 2
+#' compute component PSQI 2
 #' 
-#' Computes the second PSQI component on sleep quality, 
+#' computes the second PSQI component on sleep quality, 
 #' using the number of minutes before sleep (PSQI Q2) and
 #' subjective evaluation of sleep within 30 minutes
 #' 
-#' @param minBeforeSleep column name with no. minutes before sleep (numeric) [PSQI_02]
-#' @param noSleep30min column name with evaluation of sleep within 30min (0-3) [PSQI_05a]
+#' @param min_before_sleep column name with no. minutes before sleep (numeric) [psqi_02]
+#' @param no_sleep_30min column name with evaluation of sleep within 30min (0-3) [psqi_05a]
 #' @family psqi_functions
 #' @export
-psqi_compute_comp2 <- function(minBeforeSleep, noSleep30min){
+psqi_compute_comp2 <- function(min_before_sleep, no_sleep_30min){
   
-  tmp <- cut(minBeforeSleep, breaks = c(0, 15, 31, 61, Inf), 
+  tmp <- cut(min_before_sleep, breaks = c(0, 15, 31, 61, Inf), 
              labels = FALSE, include.lowest = TRUE, right = FALSE) - 1L
   
-  cut(tmp + noSleep30min, breaks = c(0, 1, 3, 5, Inf),
+  cut(tmp + no_sleep_30min, breaks = c(0, 1, 3, 5, Inf),
       labels = FALSE, include.lowest = TRUE, right = FALSE) - 1L
   
 }
 
-#' Compute component PSQI 3
+#' compute component PSQI 3
 #' 
-#' @param hoursSleep column name with hours of sleep (decimal hours) [PSQI_04]
+#' @param hours_sleep column name with hours of sleep (decimal hours) [psqi_04]
 #' @family psqi_functions
 #' @export
-psqi_compute_comp3 <- function(hoursSleep){
-  4L - cut(hoursSleep, breaks = c(0, 4.999, 5.999, 7, Inf), 
+psqi_compute_comp3 <- function(hours_sleep){
+  4L - cut(hours_sleep, breaks = c(0, 4.999, 5.999, 7, Inf), 
            labels = FALSE, include.lowest = TRUE, right = TRUE)
 }
 
-#' Compute component PSQI 4
+#' compute component PSQI 4
 #' 
-#' @param bedtime column name with bedtime (HH:MM:SS) [PSQI_01]
-#' @param risingtime column name with rising time (HH:MM:SS) [PSQI_03]
-#' @param hoursSleep column name with hours of sleep (decimal hours) [PSQI_04]
+#' @param bedtime column name with bedtime (HH:MM:SS) [psqi_01]
+#' @param risingtime column name with rising time (HH:MM:SS) [psqi_03]
+#' @param hours_sleep column name with hours of sleep (decimal hours) [psqi_04]
 #' @family psqi_functions
 #' @export
-psqi_compute_comp4 <- function(hoursSleep, bedtime, risingtime){
-  4L - cut(hoursSleep / compute_time_in_bed(risingtime, bedtime) * 100,
+psqi_compute_comp4 <- function(hours_sleep, bedtime, risingtime, ...){
+  4L - cut(hours_sleep / compute_time_in_bed(risingtime, bedtime, ...) * 100,
            breaks = c(0, 65, 75, 85.0001, Inf), labels = FALSE, include.lowest = TRUE,
            right = FALSE)
 }
 
-#' Compute component PSQI 5
+#' compute component PSQI 5
 #' 
 #' @param data data frame with the data
-#' @param sleepTroubles columns containing sleep problem evaluations (0-3) [PSQI_05[b-j] ]
+#' @param sleep_troubles columns containing sleep problem evaluations (0-3) [psqi_05[b-j] ]
 #' @family psqi_functions
 #' @export
-#' @importFrom dplyr enquo select mutate if_else pull
-psqi_compute_comp5 <- function(data, sleepTroubles = matches("^PSQI_05[b-j]$")){
-
-  tmp <- select(data, {{sleepTroubles}} )
-  tmp <- mutate(tmp, value = apply(tmp, 1, function(x) 
-    if_else(all(is.na(x)), NA_real_, as.numeric(sum(x, na.rm = TRUE))))
-    )
+#' @importFrom dplyr select mutate if_else pull
+psqi_compute_comp5 <- function(data, sleep_troubles = matches("^psqi_05[b-j]$")){
+  tmp <- select(data,  
+                {{sleep_troubles}})
+  tmp <- mutate(tmp,
+                value = apply(tmp, 1, function(x) 
+                  if_else(all(is.na(x)), NA_real_, as.numeric(sum(x, na.rm = TRUE))))
+  )
   tmp <- pull(tmp, value)
   
   cut(tmp, breaks = c(0, 1, 10, 19, Inf),
@@ -73,12 +78,12 @@ psqi_compute_comp5 <- function(data, sleepTroubles = matches("^PSQI_05[b-j]$")){
 
 #' Calculate component 7 of PSQI
 #' 
-#' @param keepAwake column name with evaluation of staying awake (0-3) [PSQI_08]
-#' @param keepEnthused column name with evaluation of keeping enthusiastic (0-3) [PSQI_09]
+#' @param keep_awake column name with evaluation of staying awake (0-3) [psqi_08]
+#' @param keep_enthused column name with evaluation of keeping enthusiastic (0-3) [psqi_09]
 #' @family psqi_functions
 #' @export
-psqi_compute_comp7 <- function(keepAwake, keepEnthused){
-  cut(keepAwake + keepEnthused,
+psqi_compute_comp7 <- function(keep_awake, keep_enthused){
+  cut(keep_awake + keep_enthused,
       breaks = c(-Inf, 0, 2, 4, Inf), labels = FALSE, include.lowest = TRUE, 
       right = TRUE) - 1L
 }
@@ -90,123 +95,145 @@ psqi_compute_comp7 <- function(keepAwake, keepEnthused){
 #' @family psqi_functions
 #' @inheritParams psqi_compute
 #' @export
-#' @importFrom dplyr enquo select group_by_at n row_number summarise_at pull if_else
+#' @importFrom dplyr summarise if_else select group_by
 #' @importFrom tidyr gather
-psqi_compute_global <- function(data, cols = matches("^PSQI_Comp[1-7]+_"), max_missing = 0){
-  if(max_missing > 6) stop("max_missing must be between 0 and 6.", call. = FALSE)
-
-  tmp <- select(data, {{cols}} )
-  tmp <- mutate(tmp, ID = row_number())
-  tmp <- gather(tmp, key = "key", value = "value", {{cols}} , na.rm = FALSE)
-  
-  tmp <- group_by_at(tmp, vars(ID))
-
-  tmp <- summarise_at(tmp, vars(value), 
-                      list(~ if_else(7 - sum(!is.na(.)) > max_missing, 
-                                     NA_real_, 
-                                     7 / sum(!is.na(.)) * sum(., na.rm = TRUE))))
-  pull(tmp)
+psqi_compute_global <- function(data, cols = matches("comp[1-7]+_"), max_missing = 0){
+  if(max_missing > 6) 
+    stop("max_missing must be between 0 and 6.", call. = FALSE)
+  tmp <- select(data, {{cols}})
+  tmp <- mutate(tmp, .r = 1:nrow(data))
+  tmp <- gather(tmp, 
+                key = "key", 
+                value = "value", 
+                {{cols}}, na.rm = FALSE)
+  tmp <- group_by(tmp, .r)
+  tmp <- summarise(tmp, 
+            .n = sum(!is.na(value)),
+            value = if_else(7 - .n > max_missing,
+                            NA_real_,
+                            7/.n * sum(value, na.rm = TRUE))
+            )
+  tmp$value
 }
 
-#' Compute all PSQI components and global score
+#' compute all PSQI components and global score
 #' 
 #' @param data data frame
 #'
 #' @param components integer vector of components to calculate. If all 7, global is added also
-#' @param bedtime column name with bedtime (HH:MM:SS) [PSQI_01]
-#' @param minBeforeSleep column name with no. minutes before sleep (numeric) [PSQI_02]
-#' @param risingtime column name with rising time (HH:MM:SS) [PSQI_03]
-#' @param hoursSleep column name with hours of sleep (decimal hours) [PSQI_04]
-#' @param noSleep30min column name with evaluation of sleep within 30min (0-3) [PSQI_05a]
-#' @param sleepQuality column name with evaluation of sleep quality (0-3) [PSQI_06]
-#' @param medication column name with use of sleep mediation (0-3) [PSQI_07]
-#' @param keepAwake column name with evaluation of staying awake (0-3) [PSQI_08]
-#' @param keepEnthused column name with evaluation of keeping enthusiastic (0-3) [PSQI_09]
-#' @param sleepTroubles columns containing sleep problem evaluations (0-3) [PSQI_05[a-j] ]
+#' @param bedtime column name with bedtime (HH:MM:SS) [psqi_01]
+#' @param min_before_sleep column name with no. minutes before sleep (numeric) [psqi_02]
+#' @param risingtime column name with rising time (HH:MM:SS) [psqi_03]
+#' @param hours_sleep column name with hours of sleep (decimal hours) [psqi_04]
+#' @param no_sleep_30min column name with evaluation of sleep within 30min (0-3) [psqi_05a]
+#' @param sleepquality column name with evaluation of sleep quality (0-3) [psqi_06]
+#' @param medication column name with use of sleep mediation (0-3) [psqi_07]
+#' @param keep_awake column name with evaluation of staying awake (0-3) [psqi_08]
+#' @param keep_enthused column name with evaluation of keeping enthusiastic (0-3) [psqi_09]
+#' @param sleep_troubles columns containing sleep problem evaluations (0-3) [psqi_05[a-j] ]
 #' @param max_missing Integer specifying the number of missing values to accept in the PSQI components, 
 #' before the global PSQI value is set to missing. Defaults to 0. If \code{max_missing > 0}, the 
 #' global PSQI value is computed by weighting each non-missing entry with \code{7 / (7 - max_missing)}.
-#' @param keep_all logical, append to data.frame
+#' @template keep_all
+#' @template prefix
 #'
 #' @return a data.frame containing only the calculated components
 #' @export
 #' @family psqi_functions
-#' @importFrom dplyr enquo mutate matches
+#' @importFrom dplyr mutate matches
 psqi_compute <- function(data, 
                          components = 1:7,
-                         bedtime = PSQI_01, 
-                         minBeforeSleep = PSQI_02, 
-                         risingtime = PSQI_03, 
-                         hoursSleep = PSQI_04, 
-                         noSleep30min = PSQI_05a, 
-                         sleepQuality = PSQI_06, 
-                         medication = PSQI_07, 
-                         keepAwake = PSQI_08, 
-                         keepEnthused = PSQI_09,
-                         sleepTroubles = matches("^PSQI_05[b-j]$"), 
+                         bedtime = psqi_01, 
+                         min_before_sleep = psqi_02, 
+                         risingtime = psqi_03, 
+                         hours_sleep = psqi_04, 
+                         no_sleep_30min = psqi_05a, 
+                         sleepquality = psqi_06, 
+                         medication = psqi_07, 
+                         keep_awake = psqi_08, 
+                         keep_enthused = psqi_09,
+                         sleep_troubles = matches("^psqi_05[b-j]$"), 
                          max_missing = 0,
+                         ...,
+                         prefix = "psqi_",
                          keep_all = TRUE
 ){
   
-  tmp <- data
+  tmp <- select(data, {{bedtime}}, {{min_before_sleep}},
+                {{risingtime}}, {{hours_sleep}},
+                {{no_sleep_30min}}, {{sleepquality}},
+                {{medication}}, {{keep_awake}},
+                {{keep_enthused}}, {{sleep_troubles}})
   nn <- character()
-
+  
   if(1 %in% components){
-    tmp <- mutate(tmp, PSQI_Comp1_Quality = {{sleepQuality}} )
-    nn = c(nn, "PSQI_Comp1_Quality")
+    tmp <- mutate(tmp, comp1_quality = {{sleepquality}} )
+    nn = c(nn, "comp1_quality")
   }
   
   if(2 %in% components){
     tmp <- mutate(tmp, 
-                  PSQI_Comp2_Latency = psqi_compute_comp2({{minBeforeSleep}} , 
-                                                               {{noSleep30min}} ))
-    nn = c(nn, "PSQI_Comp2_Latency")  
+                  comp2_latency = psqi_compute_comp2({{min_before_sleep}} , 
+                                                     {{no_sleep_30min}} ))
+    nn = c(nn, "comp2_latency")  
   } 
   
   if(3 %in% components){
     tmp <- mutate(tmp, 
-                  PSQI_Comp3_Duration = psqi_compute_comp3({{hoursSleep}} ))
-    nn = c(nn, "PSQI_Comp3_Duration")
+                  comp3_duration = psqi_compute_comp3({{hours_sleep}} ))
+    nn = c(nn, "comp3_duration")
   } 
   
   if(4 %in% components){
     tmp <- mutate(tmp, 
-                  PSQI_Comp4_Efficiency = psqi_compute_comp4({{hoursSleep}} , 
-                                                                  {{bedtime}},  
-                                                                  {{risingtime}} ))
-    nn = c(nn, "PSQI_Comp4_Efficiency")
+                  comp4_efficiency = psqi_compute_comp4({{hours_sleep}} , 
+                                                        {{bedtime}},  
+                                                        {{risingtime}},
+                                                        ...))
+    nn = c(nn, "comp4_efficiency")
   } 
   
   if(5 %in% components){
     tmp <- mutate(tmp, 
-                  PSQI_Comp5_Problems = psqi_compute_comp5(data, sleepTroubles))
-    nn = c(nn, "PSQI_Comp5_Problems")
+                  comp5_problems = psqi_compute_comp5(data, sleep_troubles))
+    nn = c(nn, "comp5_problems")
   } 
   
   if(6 %in% components){
     tmp <- mutate(tmp, 
-                  PSQI_Comp6_Medication = {{medication}} )
-    nn = c(nn, "PSQI_Comp6_Medication")
+                  comp6_medication = {{medication}} )
+    nn = c(nn, "comp6_medication")
   } 
   
   if(7 %in% components){
     tmp <- mutate(tmp, 
-                  PSQI_Comp7_Tired = psqi_compute_comp7({{keepAwake}} , {{keepEnthused}} ))
-    nn = c(nn, "PSQI_Comp7_Tired")
+                  comp7_tired = psqi_compute_comp7({{keep_awake}} , {{keep_enthused}} ))
+    nn = c(nn, "comp7_tired")
   } 
   
-  if(!keep_all) tmp <- select(tmp, {{nn}} )
   
   if(all(1:7 %in% components)){
-    mutate(tmp,
-           PSQI_Global = psqi_compute_global(tmp, max_missing = max_missing)
+    tmp <- mutate(tmp,
+                  global = psqi_compute_global(tmp, max_missing = max_missing)
     )
-  }else{
-    tmp
+    nn = c(nn, "global")
   }
+  
+  if(!is.null(prefix)){
+    tmp <- rename_all(tmp, ~paste0(prefix, .x))
+    nn <- paste0(prefix, nn)
+  }
+  
+  tmp <- select(tmp, {{nn}} )
+  
+  if(keep_all) 
+    tmp <- bind_cols(data, tmp)
+  
+  tmp
 }
 
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("PSQI_01", "PSQI_02", "PSQI_03", "PSQI_04", 
-                                                        "PSQI_05a", "PSQI_06", "PSQI_07", "PSQI_08", "PSQI_09",
-                                                        "value", "ID"))
+if(getRversion() >= "2.15.1")  
+  utils::globalVariables(c("psqi_01", "psqi_02", "psqi_03", "psqi_04", 
+                           "psqi_05a", "psqi_06", "psqi_07", "psqi_08", "psqi_09",
+                           "value", ".r", ".n"))
 
--- tests/testthat/test-psqi_functions.R
+++ tests/testthat/test-psqi_functions.R
@@ -1,45 +1,62 @@
-load(paste0(test_path(), "/data/psqi.rda"))
+psqi <- read.delim(test_path("data/psqi.tsv"))
 
 test_that("Check component calculations", {
   
-  expect_equal(psqi_compute_comp2(minBeforeSleep = psqi$PSQI_02, 
-                             noSleep30min = psqi$PSQI_05a),
+  tmp <- data.frame(comp2 = psqi_compute_comp2(psqi$psqi_02, psqi$psqi_05a))
+  expect_equal(tmp$comp2,
                c(0, 0, 1, 2, 2, 1, 0, 1, 1, 0, 0, 2))
   
-  expect_equal(psqi_compute_comp3(hoursSleep = psqi$PSQI_04),
+  tmp$comp3 <- psqi_compute_comp3(hours_sleep = psqi$psqi_04)
+  expect_equal(tmp$comp3,
                c(1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1))
   
-
-  expect_equal(psqi_compute_comp4(hoursSleep = psqi$PSQI_04, 
-                bedtime = psqi$PSQI_01, 
-                risingtime = psqi$PSQI_03),
+  tmp$comp4 <- psqi_compute_comp4(hours_sleep = psqi$psqi_04, 
+                                  bedtime = psqi$psqi_01, 
+                                  risingtime = psqi$psqi_03,
+                                  risingtime_func = lubridate::hms,
+                                  bedtime_func = lubridate::hms)
+  expect_equal(tmp$comp4,
                c(0, 0, 0, NA, 1, 0, 0, 0, 1, 0, 3, 2))
-
-  expect_equal(psqi_compute_comp5(data = psqi, 
-                sleepTroubles = matches("^PSQI_05[b-j]$")),
+  
+  tmp$comp5 <- psqi_compute_comp5(data = psqi, 
+                                  sleep_troubles = matches("^psqi_05[b-j]$"))
+  expect_equal(tmp$comp5,
                c(0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1))
   
-  expect_equal(psqi_compute_comp7(keepAwake = psqi$PSQI_08, 
-                keepEnthused = psqi$PSQI_09),
+  tmp$comp7 <- psqi_compute_comp7(keep_awake = psqi$psqi_08, 
+                                  keep_enthused = psqi$psqi_09)
+  expect_equal(tmp$comp7,
                c(0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0))
   
+  tmp$comp1 <- psqi$psqi_06
+  tmp$comp6 <- psqi$psqi_07
+  tmp$global <- psqi_compute_global(tmp, cols = matches("comp[1-7]+"))  
   
+  expect_equal(tmp$global,
+               c(1, 2, 5, NA, 7, 4, 1, 10, 6, 4, 7, 6)
+  )
 })
 
 
 test_that("Check component calculations", {
   
   
-  expect_equal(names(psqi_compute(psqi, keep_all = FALSE)),
-               c("PSQI_Comp1_Quality", "PSQI_Comp2_Latency", "PSQI_Comp3_Duration",
-                 "PSQI_Comp4_Efficiency", "PSQI_Comp5_Problems", "PSQI_Comp6_Medication",
-                 "PSQI_Comp7_Tired", "PSQI_Global"))
+  expect_equal(names(psqi_compute(psqi, keep_all = FALSE,
+                                  risingtime_func = lubridate::hms,
+                                  bedtime_func = lubridate::hms)),
+               c("psqi_comp1_quality", "psqi_comp2_latency", "psqi_comp3_duration",
+                 "psqi_comp4_efficiency", "psqi_comp5_problems", "psqi_comp6_medication",
+                 "psqi_comp7_tired", "psqi_global"))
   
-  expect_equal(names(psqi_compute(psqi, components = 1:3, keep_all = FALSE)),
-               c("PSQI_Comp1_Quality", "PSQI_Comp2_Latency", "PSQI_Comp3_Duration"))
+  expect_equal(names(psqi_compute(psqi, components = 1:3, keep_all = FALSE,
+                                  risingtime_func = lubridate::hms,
+                                  bedtime_func = lubridate::hms)),
+               c("psqi_comp1_quality", "psqi_comp2_latency", "psqi_comp3_duration"))
   
-  expect_equal(ncol(psqi_compute(psqi, components = 1:7, keep_all = TRUE)),
-               39)
+  expect_equal(ncol(psqi_compute(psqi, components = 1:7, keep_all = TRUE,
+                                 risingtime_func = lubridate::hms,
+                                 bedtime_func = lubridate::hms)),
+               46)
   
   expect_error(psqi_compute_global(psqi, max_missing = 8),
                "max_missing must be between 0 and 6")
